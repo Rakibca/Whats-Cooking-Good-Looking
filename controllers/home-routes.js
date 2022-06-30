3778d763c51a6f9d7231effc685af53dcef028d6
Rakibul Islam <rakib.islam@gmail.com>
2022-06-30 11:26:13 -0600

    Functionality added: Gets random recipe from spoonacular API using Axios

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,41 +1,59 @@
+const axios = require("axios");
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { User,Recipe } = require('../models');
+const {
+  User,
+  Recipe
+} = require('../models');
 
 
 // GET ON RANDOM RECIPE FROM THE API (RAKIBUL)
 router.get('/', (req, res) => {
-  Recipe.findAll({
-          attributes: [
-              'id',
-              'name',
-              'creator_id',
-              'ingredients',
-              'instruction'
-          ],
-          include: [
-              {
-                  model: User,
-                  attributes: ['username']
-              }
-          ]
-      })
-      .then(rcpData => {
-          const rcps = rcpData.map(rcp => rcp.get({plain: true}));
+  const options = {
+    method: 'GET',
+    url: 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random',
+    params: {
+      number: '1',
+      tags: 'vegetarian,dessert' //Choose the types of recipe randomly by using tag names
+    },
+    headers: {
+      'X-RapidAPI-Key': '6092f415c9msh596be8e63a564f4p162e5ajsn61ab17493dba',
+      'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
+    }
+  };
+  axios.request(options)
+    .then(rcpData => {
+      rcpArray = [];
+      rcpArray.push(rcpData.data.recipes[0].title);
+      //console.log(rcpData.data.recipes[0].title);
+      const arrayKeys = Object.keys(rcpData.data.recipes[0].extendedIngredients);
+      const numberOfIngredients = arrayKeys.length;
+      for (i = 0; i < numberOfIngredients; i++) {
+        rcpArray.push(rcpData.data.recipes[0].extendedIngredients[i].originalName);
+        //console.log(rcpData.data.recipes[0].extendedIngredients[i].originalName);
+      }
+      rcpArray.push(rcpData.data.recipes[0].instructions);
+      //console.log(rcpData.data.recipes[0].instructions);
+      const rcps = rcpArray;
+      //console.log(rcps);
 
-          res.render('homepage', {rcps, loggedIn: req.session.logged_in});
-      })
-      .catch(err => {
-          console.log(err);
-          res.status(500).json(err);
+      //Render to homepage
+      res.render('homepage', {
+        rcps,
+        loggedIn: req.session.logged_in
       });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // LOGIN route (TONY)
 router.get('/login', (req, res) => {
   if (req.session.logged_in) {
-      res.redirect('/');
-      return;
+    res.redirect('/');
+    return;
   }
   res.render('login');
 });
@@ -43,8 +61,8 @@ router.get('/login', (req, res) => {
 // SIGNUP route (TONY)
 router.get('/signup', (req, res) => {
   if (req.session.logged_in) {
-      res.redirect('/');
-      return;
+    res.redirect('/');
+    return;
   }
 
   res.render('signup');
